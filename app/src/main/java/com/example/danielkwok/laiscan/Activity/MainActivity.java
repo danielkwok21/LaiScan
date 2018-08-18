31649759af837371355c98a71d48c05e9602a0fe
danielkwok21 <danielzhengxian21@gmail.com>
2018-08-19 02:44:39 +0800

    google play services added

--- app/src/main/java/com/example/danielkwok/laiscan/Activity/MainActivity.java
+++ app/src/main/java/com/example/danielkwok/laiscan/Activity/MainActivity.java
@@ -28,9 +28,9 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        main_scan_btn = findViewById(R.id.main_scan_btn);
-        main_long_t = findViewById(R.id.main_long_t);
-        main_lat_t = findViewById(R.id.main_lat_t);
+        main_scan_btn = (Button) findViewById(R.id.main_scan_btn);
+        main_long_t = (TextView) findViewById(R.id.main_long_t);
+        main_lat_t =  (TextView) findViewById(R.id.main_lat_t);
         QRScan = new IntentIntegrator(this);
 
         main_scan_btn.setOnClickListener(new View.OnClickListener() {
