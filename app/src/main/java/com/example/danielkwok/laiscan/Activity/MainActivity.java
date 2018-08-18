287471cca992f7661127683d739976078a80f5a6
danielkwok21 <danielzhengxian21@gmail.com>
2018-08-18 22:03:07 +0800

    ui and library

--- app/src/main/java/com/example/danielkwok/laiscan/Activity/MainActivity.java
+++ app/src/main/java/com/example/danielkwok/laiscan/Activity/MainActivity.java
@@ -4,12 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.danielkwok.laiscan.R;
+import com.google.zxing.Result;
 
-public class MainActivity extends AppCompatActivity {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
